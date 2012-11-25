3bb6eab99ea0713bbc2e583b41bcc6d1f03803a3
Andrew Knapp <acmagius@hotmail.com>
2012-11-24 23:55:23 -0800

    reset head

--- contentDownloader.py
+++ contentDownloader.py
@@ -3,29 +3,34 @@ import BeautifulSoup
 import sys
 import urlparse
 
-site = sys.argv[1] 
-fileTypes = ["pdf", "doc", "cvs", "jpeg", "jpg", "avi", "mpeg", "rm", "mpg", 		
-		"mp4", "wmv", "rm", "ram", "mov", "mp3"] 
+SITE = sys.argv[1]
+FILE_TYPES = ["pdf", "doc", "cvs", "jpeg", "jpg", "avi", "mpeg", "rm", "mpg",
+              "mp4", "wmv", "rm", "ram", "mov", "mp3"]
 
-def checkFileType(link, extension):
-	return extension in fileTypes
 
-def downloadContent(site):
-	try:
-		oSite = urllib2.urlopen(site).read()
-		soup = BeautifulSoup.BeautifulSoup(oSite)
-		n = 1
-		for page in soup.findAll("a"):
-			lnk = page['href'].encode('latin-1')
-			lnk2 = urlparse.urljoin(site, lnk)
-			temp = lnk2.rsplit('.', 1)
-			if checkFileType(lnk2, temp[1]):
-				print "Downloading file" + str(n)
-				download = urllib2.urlopen(lnk2).read()
-				file = open('file' + str(n) + "." + temp[1], 'w')
-				file.write(download)
-				n += 1
-	except:
-		print site + ": could not be downloaded"
+def check_file_type(link, extension):
+    '''Checks for known file types'''
+    return extension in FILE_TYPES
 
-downloadContent(site)
+
+def download_content(site):
+    '''Checks each link on a site to see if it is a known filetype'''
+    try:
+        oSite = urllib2.urlopen(site).read()
+        soup = BeautifulSoup.BeautifulSoup(oSite)
+        n = 1
+        for page in soup.findAll("a"):
+            lnk = page['href'].encode('latin-1')
+            lnk2 = urlparse.urljoin(site, lnk)
+            temp = lnk2.rsplit('.', 1)
+            if check_file_type(lnk2, temp[1]):
+                print "Downloading file" + str(n)
+                download = urllib2.urlopen(lnk2).read()
+                file = open('file' + str(n) + "." + temp[1], 'w')
+                file.write(download)
+                n += 1
+    except:
+        print site + ": could not be downloaded"
+
+
+download_content(site)
